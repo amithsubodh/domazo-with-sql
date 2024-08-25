bd09977bc617a6434f3ec8d417698f40c5c710b0
H G Pratheek Gowda <gowdapratheek91@gmail.com>
2024-08-25 10:14:45 +0530

    server

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -1,4 +1,4 @@
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
     const Contact = sequelize.define("Contact", {
       name: {
         type: DataTypes.STRING,
